164f976c3023af802af8c8a74ddd332125a299f9
Andrew D'Ambrosio <adambrosio20@gmail.com>
2021-03-01 11:51:16 -0500

    full check board func

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -63,3 +63,12 @@ CHECKS IF SPACE IS AVAILABLE
 '''
 def space_check(board, position):
     return board[position] == ' '
+
+'''
+FULL BOARD CHECK
+'''
+def full_board_check(board):
+    for i in range(1, 10):
+        if space_check(board, i):
+            return False
+    return True
