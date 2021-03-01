f89d70bd2334d3a556f500446e0168137ee9d6c8
Andrew D'Ambrosio <adambrosio20@gmail.com>
2021-03-01 11:56:09 -0500

    player_choice func

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -72,3 +72,15 @@ def full_board_check(board):
         if space_check(board, i):
             return False
     return True
+
+'''
+USES space_check() TO CHECK USER INPUT AGAINST THE BOARD
+'''
+def player_choice(board):
+
+    position = 0
+
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+        position = int('Choose a position (1-9): ')
+
+    return position
