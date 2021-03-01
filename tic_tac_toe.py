004c999cd0f4f1e1cae46702029c37120f5cfd7b
Andrew D'Ambrosio <adambrosio20@gmail.com>
2021-03-01 11:03:14 -0500

    takes user input and assigns their choice to either X or O

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,3 +8,18 @@ def display_board(board):
     print(board[4] + '|' + board[5] + '|' + board[6])
     print('-----')
     print(board[7] + '|' + board[8] + '|' + board[9])
+
+def player_input():
+
+    '''
+    OUTPUT = (Player 1 Marker, Player 2 Marker)
+    '''
+
+    marker = ''
+
+    while marker != 'X' and marker != 'O':
+        marker = input('Player 1: Choose X or O').upper()
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
