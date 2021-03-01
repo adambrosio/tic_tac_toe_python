5da5a3f71dfae2ff39cad748db4ce97951b48a8c
Andrew D'Ambrosio <adambrosio20@gmail.com>
2021-03-01 11:18:55 -0500

    function detects winner using indexing. tested.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -30,3 +30,16 @@ TAKES IN BOARD LIST OBJECT, THE MARKER ('X OR 'O), AND THE DESIRED POISITION (1-
 '''
 def place_marker(board, marker, position):
     board[position] = marker
+
+'''
+CHECKS FOR WINNER USING INDEXING
+'''
+def win_check(board, mark):
+    return ((board[1] == mark and board[2] == mark and board[3]) == mark or
+    (board[4] == mark and board[5] == mark and board[6] == mark) or 
+    (board[7] == mark and board[8] == mark and board[9] == mark) or
+    (board[1] == mark and board[5] == mark and board[9] == mark) or
+    (board[3] == mark and board[5] == mark and board[7] == mark) or
+    (board[2] == mark and board[5] == mark and board[8] == mark) or
+    (board[1] == mark and board[4] == mark and board[7] == mark) or
+    (board[3] == mark and board[6] == mark and board[9] == mark))  
