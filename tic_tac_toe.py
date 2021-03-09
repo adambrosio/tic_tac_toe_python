70e03e7a0f0b3a4c32d877c85f5c2430a410c1d7
Andrew D'Ambrosio <adambrosio20@gmail.com>
2021-03-09 14:50:48 -0500

    type-o

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,10 +1,10 @@
 '''
 JUPYTER ISSUE?
 '''
-from IPython.display import clear_output
+#from IPython.display import clear_output
 
 def display_board(board):
-    clear_output()
+    #clear_output()
 
     print(board[1] + '|' + board[2] + '|' + board[3])
     print('-----')
